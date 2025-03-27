4dd586143283f892f1f72f85b80b7be13a1cfdad
sashavershkova <a.m.vershkova@gmail.com>
2025-03-27 11:36:07 -0700

    create class Student, add get more classes func

--- activity/student.py
+++ activity/student.py
@@ -1,1 +1,25 @@
 # add your Student class here!
+
+class Student():
+    def __init__(self, name, grade, classes = []):
+        self.name = name
+        self.grade = grade
+        self.classes = classes
+
+    def add_class(self, class_name):
+        self.classes.append(class_name)
+        
+    def get_num_classes():
+        return len(self.classes)
+    
+    def summary():
+        print(f"{self.name} is a {self.grade} enrolled in {self.get_num_classes()} classes")
+
+    
+    
+
+
+    
+
+
+
